5d824d0268d57fe7e02eeb49494b121c6243eff2
Aluno <aluno@lab71-maq14.ifalpalmeira.edu.br>
2019-09-10 11:26:21 -0300

    Contribuição de Atônio Elvis, turma 514

--- calculadora.py
+++ calculadora.py
@@ -1,27 +1,36 @@
 #Complete as funcoes a seguir
 
-def soma(a, b):
-	#Insira o codigo aqui
+while True:
+	def soma(a , b):
+		#Insira o código aqui
+		print(" a + b = ", a+b)
 
-def subtrai(a, b):
-	#Insira o codigo aqui
+	def subtrai(a , b):
+		#Insira o código aqui
+		print("a - b = ", a-b)
 
-def multiplica(a, b):
-	#Insira o codigo aqui
+	def multiplica(a , b):
+		#Insira o código aqui
 
-def divide(a, b):
-	#Insira o codigo aqui
+		print("a * b = " , a*b)
 
+	def divide(a , b):
+		#Insira o código aqui
+		if(b != 0):
+			print("a / b = " , a/b)
+		else:
+			print("Não é possível fazer esta operação, 'b' tem que ser diferente de 0 !")
+		
+	#Programa principal
 
-#Programa principal
+	print("Calculadora simples")
 
-print("Calculadora simples")
+	num1 = float(input("Insira o primeiro numero: "))
+	num2 = float(input("Insira o segundo numero: "))
 
-num1 = float(input("Insira o primeiro numero: "))
-num2 = float(input("Insira o segundo numero: "))
+	soma(num1, num2)
+	subtrai(num1, num2)
+	multiplica(num1, num2)
+	divide(num1, num2)
 
-soma(num1, num2)
-subtrai(num1, num2)
-multiplica(num1, num2)
-divide(num1, num2)
 
